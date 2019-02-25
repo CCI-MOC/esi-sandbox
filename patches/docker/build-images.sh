1ebf042bc161d9ba5edb6f444926808a51313a0c
Tzu-Mainn Chen <tzumainn@redhat.com>
2019-02-25 19:27:58 +0000

    add mistral images to support baremetal node trait manipulation

--- patches/docker/build-images.sh
+++ patches/docker/build-images.sh
@@ -3,6 +3,8 @@
 set -e
 declare -A images
 
+images[mistral-api]=192.168.1.2:8787/moc/centos-binary-mistral-api:rocky-ironic-traits
+images[mistral-executor]=192.168.1.2:8787/moc/centos-binary-mistral-executor:rocky-ironic-traits
 images[nova-compute]=192.168.1.2:8787/moc/centos-binary-nova-compute:I907b69eb689cf6c169a4869cfc7889308ca419d5
 images[nova-compute-ironic]=192.168.1.2:8787/moc/centos-binary-nova-compute-ironic:I907b69eb689cf6c169a4869cfc7889308ca419d5
 
