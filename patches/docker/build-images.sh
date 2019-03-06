85a69f203603fd3a7a77ef6792e27ff0582368d9
Tzu-Mainn Chen <tzumainn@redhat.com>
2019-03-06 23:54:19 +0000

    add nova-scheduler image that includes oesi filter

--- patches/docker/build-images.sh
+++ patches/docker/build-images.sh
@@ -7,6 +7,7 @@ images[mistral-api]=192.168.1.2:8787/moc/centos-binary-mistral-api:rocky-ironic-
 images[mistral-executor]=192.168.1.2:8787/moc/centos-binary-mistral-executor:rocky-ironic-traits
 images[nova-compute]=192.168.1.2:8787/moc/centos-binary-nova-compute:I907b69eb689cf6c169a4869cfc7889308ca419d5
 images[nova-compute-ironic]=192.168.1.2:8787/moc/centos-binary-nova-compute-ironic:I907b69eb689cf6c169a4869cfc7889308ca419d5
+images[nova-scheduler]=192.168.1.2:8787/moc/centos-binary-nova-scheduler:rocky-oesi-scheduler
 
 for k in "${!images[@]}"; do
 	echo "building in $k"
